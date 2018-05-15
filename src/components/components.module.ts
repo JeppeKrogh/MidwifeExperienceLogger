7d8179f23997a5ec63bcd32cea22946f8cedc642
Tha Dah Baw <1055340@ucn.dk>
2018-05-15 21:06:36 +0200

    commit

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { GoogleLoginComponent } from './google-login/google-login';
 import { PopoversComponent } from './popovers/popovers';
-import { IntroSkipSlidesComponent } from './intro-skip-slides/intro-skip-slides';
 
 
 @NgModule({
 	declarations: [GoogleLoginComponent,
-    PopoversComponent,
-    IntroSkipSlidesComponent],
-	imports: [],
-	exports: [GoogleLoginComponent,
-    PopoversComponent,
-    IntroSkipSlidesComponent]
-})
+        PopoversComponent],
+        imports: [],
+        exports: [GoogleLoginComponent,
+        PopoversComponent]
+    })
 export class ComponentsModule {}
