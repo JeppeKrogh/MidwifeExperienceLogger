eb427f7d22e49ed9cd956067c30478b2856b3bf1
Tha Dah Baw <1055340@ucn.dk>
2018-05-17 22:29:09 +0200

    commit  date 17-05-2018

--- src/pages/introslides/introslides.ts
+++ src/pages/introslides/introslides.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, Slides, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the IntroslidesPage page.
@@ -14,8 +14,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'introslides.html',
 })
 export class IntroslidesPage {
+  @ViewChild(Slides) slides:Slides;
+  skipMsg: string ="Skip";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  skip() {
+    this.navCtrl.push(IntroslidesPage);
+  }
+
+  slideChanged() {
+    if(this.slides.isEnd())
+    this.skipMsg = "Alright, i got it";
+
   }
 
   ionViewDidLoad() {
