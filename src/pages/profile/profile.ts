72112aeeab6d18c9e469fab06b02d7866ea202e0
Jeppe Krogh <jeppejulius@hotmail.com>
2018-05-23 16:30:18 +0200

    Ny opdateret master med det nyeste frontend

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { Subscription } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import { Chart} from 'chart.js';
 
 /**
  * Generated class for the ProfilePage page.
@@ -18,8 +19,12 @@ import 'firebase/firestore';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth) {
+  @ViewChild('barCanvas') barCanvas;
+ 
+  barChart: any;
+  doughnutChart: any;
+  lineChart: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth, public myModal:ModalController) {
 
 
     this.afAuth.authState.subscribe(res => {
@@ -44,6 +49,43 @@ export class ProfilePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+
+
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+
+      type: 'bar',
+      data: {
+          labels: ["1", "2", "3","4", "5", "6"],
+          backgroundColor:'rgba(255, 99, 132, 0.2)',
+          borderColor:'rgba(255, 99, 132, 0.2)',
+          datasets: [{
+              label: '# of days',
+              data: [12, 19, 3, 5, 2, 3, 4, 7, 4, 2],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }],
+              xAxes: [{
+                gridLines: {
+                    offsetGridLines: true
+                }
+            }]
+          }
+      }
+
+  });
+  }
+  showModal() {
+   
+    const modal = this.myModal.create('ModalContentPage');
+    modal.present();
+
   }
 
 }
